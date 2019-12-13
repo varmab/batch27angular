ee1c381f0b0c6f45f073e6866b12c58b30805296
Varma Bhupatiraju <varmab@gmail.com>
2019-12-13 10:55:47 +0530

    latest code

--- app/greeting.component.ts
+++ app/greeting.component.ts
@@ -3,15 +3,32 @@ import {Component} from '@angular/core'
 @Component({
     selector:'greeting',
     template:`
-        <h1>Happy New Year, {{ name }}</h1>
-        <input type="text" [value]="name"/>
+        <h1 *ngIf="name.length>0">Happy New Year, {{ name }}</h1>
+        <input type="text" (input)="handleNameChange($event)" [value]="name"/>
+        <h1>Happy Pet, {{ petName }}</h1>
+        <input type="text" [(ngModel)]="petName"/>
         <button (click)="updateName($event)">Update Name</button>
+        <ul>
+            <li *ngFor="let friend of friends; let i=index"
+                [ngStyle]="{color:((i+1)%2===0)?'red':'green'}"
+            >
+               {{ i+1 }} Happy New Year, {{ friend }}
+            </li>
+        </ul>
     `
 })
 export class GreetingComponent{
     name:string="Varma"
+    petName:string="Monty";
+    friends:string[]=["Ram","Robert","Rahim"];
 
-    updateName(){
+
+    updateName(event){
         this.name="Ram";
+    }
+
+    handleNameChange(e){
+        var newName=e.target.value;
+        this.name=newName;
     }
 }
